ad22ad3e171ba4a26a599b411a31a0e801fd9845
Maxi Jaeger <jagermaxi1@gmail.com>
2021-12-22 12:59:07 +0100

    commit

---
+++ WebIO_Playstore_Kubinger_Jaeger/WebIO_Playstore_Kubinger_Jaeger/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebIO_Playstore_Kubinger_Jaeger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
